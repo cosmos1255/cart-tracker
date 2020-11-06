857d2bb4fe5f49e183a8070849c8a0c10b1906db
David Eyrich <davidseyrich@outlook.com>
2020-11-06 08:55:51 -0500

    added new file for main app functionality

---
+++ App/MainFunctionality.java
@@ -0,0 +1,13 @@
+// David Eyrich - Apache License 2.0
+// 
+// 11-06-2020 00:01:04am
+//
+// To run: javac MainFunctionality.java -> java MainFunctionality
+
+public class MainFunctionality
+{
+    public static void main(String[] args)
+    {
+        System.out.println("MainFunctionality.java");
+    }
+}
